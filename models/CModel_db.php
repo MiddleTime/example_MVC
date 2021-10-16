74d653dbfad777abfa62a64377c73738b0279604
Sergei <nadoroge@gmail.com>
2021-10-16 20:36:07 +0300

    example ready

---
+++ models/CModel_db.php
@@ -0,0 +1,40 @@
+<?php
+
+class Cmodel {
+    public function GetData(){
+
+        $mysqli = new mysqli('localhost', 'root', '', 'blog');
+
+        if(mysqli_connect_errno()) {
+			print_f('Соедение не установлено');
+			exit();
+		}
+
+        $mysqli->set_charset('utf8');
+
+		$query = "Select * From News";
+
+		$result = $mysqli->query($query);
+		$arrayResult = $result->fetch_all(MYSQLI_ASSOC);
+
+       
+		
+        // $arrayResult[] = array(
+        //     'TITLE' => 'Blog hosting site',
+        //     'DATE' => '22.06.2020',
+        //     'AUTHOR' => 'VolDeMort',
+        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
+        //     'TEXT' => 'Nulla morbi, ad pur Portwisu morbsed id, arcu vestibulum dolor conmtum ac egestas lorem elit libero.'   
+
+        //  );
+        // $arrayResult[] = array(
+        //     'TITLE' => 'Blog hosting site NEW',
+        //     'DATE' => '21.10.2021',
+        //     'AUTHOR' => 'Igor',
+        //     'IMAGE' => 'https://news.artnet.com/app/news-upload/2019/01/Cat-Photog-Feat-256x256.jpg',
+        //     'TEXT' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus harum voluptas esse vel cumque culpa.'   
+
+        //  );
+         return $arrayResult;
+    }
+}
